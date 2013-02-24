a5dcde5ebbd11d0cf1b194ac63ad3ede4d0d102a
blha303 <stevensmith.ome@gmail.com>
2013-02-24 08:05:51 +0800

    config fix

--- src/me/blha303/DemiGods.java
+++ src/me/blha303/DemiGods.java
@@ -21,13 +21,14 @@ import com.gmail.nossr50.skills.utilities.SkillType;
 
 public class DemiGods extends JavaPlugin implements Listener {
 	private GroupManager groupManager;
-	
+
 	public void onEnable() {
 		final PluginManager pluginManager = this.getServer().getPluginManager();
 		final Plugin GMplugin = pluginManager.getPlugin("GroupManager");
- 
-		if (GMplugin != null && GMplugin.isEnabled())
-		{ groupManager = (GroupManager)GMplugin; }
+
+		if (GMplugin != null && GMplugin.isEnabled()) {
+			groupManager = (GroupManager) GMplugin;
+		}
 		List<String> grouplist = new ArrayList<String>();
 		grouplist.add("zeus");
 		grouplist.add("ares");
@@ -37,66 +38,77 @@ public class DemiGods extends JavaPlugin implements Listener {
 		grouplist.add("hades");
 		grouplist.add("aphrodite");
 		grouplist.add("poseidon");
-		
+
 		getConfig().addDefault("restrictNumberOfChanges", true);
 		getConfig().addDefault("listOfGroups", grouplist);
 		getConfig().addDefault("numberOfChanges", 7);
-		getConfig().addDefault("errorOnChangeLimitExceeded", "You've exceeded the path change limit.");
-		getConfig().addDefault("pathChangeSuccessful", "Your path has been changed to honour %groupname%!");
-		getConfig().addDefault("invalidPath", "Invalid path name! Use /path to see the list.");
-                getConfig().addDefault("zeusBoost", 0.05f);
-                getConfig().addDefault("aresBoost", 0.09f);
-                getConfig().addDefault("hadesBoost", 0.17f);
-                getConfig().addDefault("aphroditeBoost", 0.17f);
-                getConfig().addDefault("artemisBoost", 0.15f);
-                getConfig().addDefault("apolloBoost", 0.15f);
-                getConfig().addDefault("athenaBoost", 0.15f);
-                getConfig().addDefault("poseidonBoost", 0.20f);
+		getConfig().addDefault("errorOnChangeLimitExceeded",
+				"You've exceeded the path change limit.");
+		getConfig().addDefault("pathChangeSuccessful",
+				"Your path has been changed to honour %groupname%!");
+		getConfig().addDefault("invalidPath",
+				"Invalid path name! Use /path to see the list.");
+		getConfig().addDefault("zeusBoost", "0.05");
+		getConfig().addDefault("aresBoost", "0.09");
+		getConfig().addDefault("hadesBoost", "0.17");
+		getConfig().addDefault("aphroditeBoost", "0.17");
+		getConfig().addDefault("artemisBoost", "0.15");
+		getConfig().addDefault("apolloBoost", "0.15");
+		getConfig().addDefault("athenaBoost", "0.15");
+		getConfig().addDefault("poseidonBoost", "0.20");
 		getConfig().addDefault("separator", "&f, &c");
 		getConfig().options().copyDefaults(true);
 		saveConfig();
-		
+
 		this.getServer().getPluginManager().registerEvents(this, this);
 	}
 
 	@SuppressWarnings("unchecked")
-	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+	public boolean onCommand(CommandSender sender, Command cmd, String label,
+			String[] args) {
 		Player player;
 		if (sender instanceof Player) {
-			player = (Player)sender;
+			player = (Player) sender;
 		} else {
 			player = null;
 		}
-		
+
 		if (player != null) {
 			if (getConfig().getInt(player.getName() + ".changes") > 7) {
-				player.sendMessage(ChatColor.translateAlternateColorCodes
-						('&', getConfig().getString("errorOnChangeLimitExceeded")));
+				player.sendMessage(ChatColor.translateAlternateColorCodes('&',
+						getConfig().getString("errorOnChangeLimitExceeded")));
 				return true;
 			}
 			if (args.length == 1) {
 				if (args[0].equalsIgnoreCase("list")) {
 					String list = "";
-					for (String name : (List<String>)getConfig().getList("listOfGroups")) {
+					for (String name : (List<String>) getConfig().getList(
+							"listOfGroups")) {
 						if (list == "") {
 							list = name;
 						} else {
-							list = list + getConfig().getString("separator") + name;
+							list = list + getConfig().getString("separator")
+									+ name;
 						}
 					}
-					player.sendMessage(ChatColor.translateAlternateColorCodes('&', list)); 
-					return true; 
+					player.sendMessage(ChatColor.translateAlternateColorCodes(
+							'&', list));
+					return true;
 				} else {
-					
+
 					if (!setGroup(player, args[0])) {
 						player.sendMessage(getConfig().getString("invalidPath"));
 						return true;
 					} else {
-						getConfig().set(player.getName() + ".changes", getConfig().getInt(player.getName() + ".changes") + 1);
+						getConfig().set(
+								player.getName() + ".changes",
+								getConfig().getInt(player.getName() + ".changes") + 1);
 						saveConfig();
-						player.sendMessage(getConfig().getString("pathChangeSuccessful").replaceAll("%groupname%", getGroup(player)));
+						player.sendMessage(getConfig().getString(
+								"pathChangeSuccessful").replaceAll(
+								"%groupname%", getGroup(player)));
 						return true;
-					} 
+					}
 				}
 			} else {
 				return false;
@@ -106,36 +118,35 @@ public class DemiGods extends JavaPlugin implements Listener {
 			return false;
 		}
 	}
-	
-	//GroupManager methods
-	public String getGroup(final Player base)
-	{
-		final AnjoPermissionsHandler handler = groupManager.getWorldsHolder().getWorldPermissions(base);
-		if (handler == null)
-		{
+
+	// GroupManager methods
+	public String getGroup(final Player base) {
+		final AnjoPermissionsHandler handler = groupManager.getWorldsHolder()
+				.getWorldPermissions(base);
+		if (handler == null) {
 			return null;
 		}
 		return handler.getGroup(base.getName());
 	}
- 
-	public boolean setGroup(final Player base, final String group)
-	{
-		final OverloadedWorldHolder handler = groupManager.getWorldsHolder().getWorldData(base);
-		if (handler == null)
-		{
+
+	public boolean setGroup(final Player base, final String group) {
+		final OverloadedWorldHolder handler = groupManager.getWorldsHolder()
+				.getWorldData(base);
+		if (handler == null) {
 			return false;
 		}
-	
+
 		try {
-	handler.getUser(base.getName()).setGroup(handler.getGroup(group));
-} catch (NullPointerException name) {
-	return false;
-}
+			handler.getUser(base.getName()).setGroup(handler.getGroup(group));
+		} catch (NullPointerException name) {
+			return false;
+		}
 		return true;
 	}
-	//End GroupManager methods
-	
-	//MCMMO methods
+
+	// End GroupManager methods
+
+	// MCMMO methods
 	@EventHandler
 	public void onPlayerGetXP(final McMMOPlayerXpGainEvent event) {
 		Player player = event.getPlayer();
@@ -143,72 +154,78 @@ public class DemiGods extends JavaPlugin implements Listener {
 		int xp = event.getXpGained();
 		String group = getGroup(player);
 		if (group.equalsIgnoreCase("zeus")) {
-			float newxp = xp + (0.05f * xp);
+			float newxp = xp + (Float.parseFloat(getConfig().getString("zeusBoost")) * xp);
 			event.setXpGained(Math.round(newxp));
 			return;
 		} else if (group.equalsIgnoreCase("ares")) {
 			switch (skill) {
-			case UNARMED: case SWORDS: case ARCHERY: case AXES:
-				float newxp = xp + (0.09f * xp);
+			case UNARMED:
+			case SWORDS:
+			case ARCHERY:
+			case AXES:
+				float newxp = xp + (Float.parseFloat(getConfig().getString("aresBoost")) * xp);
 				event.setXpGained(Math.round(newxp));
 				break;
 			default:
 				return;
-			}	
+			}
 		} else if (group.equalsIgnoreCase("artemis")) {
 			switch (skill) {
-			case TAMING: case FISHING:
-				float newxp = xp + (0.15f * xp);
+			case TAMING:
+			case FISHING:
+				float newxp = xp + (Float.parseFloat(getConfig().getString("artemisBoost")) * xp);
 				event.setXpGained(Math.round(newxp));
 				break;
 			default:
 				return;
-			}	
+			}
 		} else if (group.equalsIgnoreCase("athena")) {
 			switch (skill) {
-			case ACROBATICS: case REPAIR:
-				float newxp = xp + (0.15f * xp);
+			case ACROBATICS:
+			case REPAIR:
+				float newxp = xp + (Float.parseFloat(getConfig().getString("athenaBoost")) * xp);
 				event.setXpGained(Math.round(newxp));
 				break;
 			default:
 				return;
-			}	
+			}
 		} else if (group.equalsIgnoreCase("apollo")) {
 			switch (skill) {
-			case WOODCUTTING: case HERBALISM:
-				float newxp = xp + (0.15f * xp);
+			case WOODCUTTING:
+			case HERBALISM:
+				float newxp = xp + (Float.parseFloat(getConfig().getString("apolloBoost")) * xp);
 				event.setXpGained(Math.round(newxp));
 				break;
 			default:
 				return;
-			}	
+			}
 		} else if (group.equalsIgnoreCase("hades")) {
 			switch (skill) {
 			case MINING:
-				float newxp = xp + (0.17f * xp);
+				float newxp = xp + (Float.parseFloat(getConfig().getString("hadesBoost")) * xp);
 				event.setXpGained(Math.round(newxp));
 				break;
 			default:
 				return;
-			}	
+			}
 		} else if (group.equalsIgnoreCase("aphrodite")) {
 			switch (skill) {
 			case EXCAVATION:
-				float newxp = xp + (0.17f * xp);
+				float newxp = xp + (Float.parseFloat(getConfig().getString("aphroditeBoost")) * xp);
 				event.setXpGained(Math.round(newxp));
 				break;
 			default:
 				return;
-			}	
+			}
 		} else if (group.equalsIgnoreCase("poseidon")) {
 			switch (skill) {
 			case EXCAVATION:
-				float newxp = xp + (0.20f * xp);
+				float newxp = xp + (Float.parseFloat(getConfig().getString("poseidonBoost")) * xp);
 				event.setXpGained(Math.round(newxp));
 				break;
 			default:
 				return;
-			}	
+			}
 		} else {
 			player.sendMessage("Please choose a god to honour. Use /path");
 		}
